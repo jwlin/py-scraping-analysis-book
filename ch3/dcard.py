611ca8be90613261a3a429129d1dd19b06226d8d
jwlin <cs.castman@gmail.com>
2019-01-25 08:07:02 -0800

    update headers for dcard

--- ch3/dcard.py
+++ ch3/dcard.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 
 def main():
     URL = 'https://www.dcard.tw/f'
-    resp = requests.get(URL)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    resp = requests.get(URL, headers=headers)
     soup = BeautifulSoup(resp.text, 'html.parser')
 
     articles = []
