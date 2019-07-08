cd450dec51e5dea531f3ba315eaf8eff456f5f5a
jwlin <cs.castman@gmail.com>
2019-07-08 00:55:35 -0700

    update to ch7/thrsc.py

--- ch7/thrsc.py
+++ ch7/thrsc.py
@@ -41,20 +41,47 @@ def time_table(start_station, end_station, search_date, search_time, rest_time=N
     return times, prev_time, next_time
 
 
+def get_times(start_station, end_station, search_date, search_time):
+    URL = 'https://www.thsrc.com.tw/tw/TimeTable/Search'
+    form_data = {
+        'StartStationName': start_station,#'台北站',
+        'EndStationName': end_station,#'嘉義站',
+        'SearchType': 'S',
+        'StartStation': station_to_id[start_station],# '977abb69-413a-4ccf-a109-0272c24fd490',
+        'EndStation': station_to_id[end_station],# '60831846-f0e4-47f6-9b5b-46323ebdcef7',
+        'DepartueSearchDate': search_date,#'2019/07/07',
+        'DepartueSearchTime': search_time#'15:30'
+    }
+    resp = requests.post(URL, data=form_data)
+    data = resp.json()
+    # 回傳資料也包含其他車次詳細資訊, 此處僅取出其中四項作為範例
+    columns = ['TrainNumber', 'DepartureTime', 'DestinationTime', 'Duration']
+    times = []
+    for d in data['data']['DepartureTable']['TrainItem']:
+        times.append([d[c] for c in columns])
+    return times
+
+
 if __name__ == '__main__':
     station_to_id = get_station_id()
-    all_times = []
-
-    times, prev_time, next_time = time_table(station_to_id['台北站'], station_to_id['嘉義站'], '2018/07/15', '10:30')
-    all_times += times
-
-    # 較早班次
-    times, _, _ = time_table(station_to_id['台北站'], station_to_id['嘉義站'], '2018/07/15', '10:30', prev_time, '1')
-    all_times = times + all_times
-
-    # 較晚班次
-    times, _, _ = time_table(station_to_id['台北站'], station_to_id['嘉義站'], '2018/07/15', '10:30', next_time, '2')
-    all_times += times
-
-    for t in all_times:
+    times = get_times('台北站', '嘉義站', '2019/07/12', '10:30')
+    print('車次, 出發時間, 抵達時間, 行車時間')
+    for t in times:
         print(t)
+
+    # 20190708: 因高鐵網頁查詢機制改變, 以下程式碼已不適用
+    # all_times = []
+    #
+    # times, prev_time, next_time = time_table(station_to_id['台北站'], station_to_id['嘉義站'], '2018/07/15', '10:30')
+    # all_times += times
+    #
+    # # 較早班次
+    # times, _, _ = time_table(station_to_id['台北站'], station_to_id['嘉義站'], '2018/07/15', '10:30', prev_time, '1')
+    # all_times = times + all_times
+    #
+    # # 較晚班次
+    # times, _, _ = time_table(station_to_id['台北站'], station_to_id['嘉義站'], '2018/07/15', '10:30', next_time, '2')
+    # all_times += times
+    #
+    # for t in all_times:
+    #     print(t)
--- update.md
+++ update.md
@@ -1,3 +1,4 @@
+* 20190708: `ch7/thrsc.py`: 因高鐵網頁查詢機制改變, 原程式碼已不適用; 該網頁新的機制會送出 POST 請求到另一個網址, 並回傳 JSON 檔案, 包含當日所有車次資料. 請參考新的 `get_times()` 函式
 * 20190523: `ch8/finanance/kd.py`: 修正程式碼關於取得前一天 k, d 值的錯誤
 * 20190309: `ch3/news.py`: 擷取自由今日焦點時加上判斷式 `if ele.find('a', 'tit')` 以略過廣告區塊
 * 20190125: `ch3/dcard.py`: `requests.get()` 內加上 User-Agent 的 header
